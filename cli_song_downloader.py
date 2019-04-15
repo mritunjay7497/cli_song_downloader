53ac8836d8f2ae98fcd54eccea638e38c6803837
mritunjay kumar mani <mkumarmani97@gmail.com>
2019-04-15 11:20:50 +0530

    Update cli_song_downloader.py

--- cli_song_downloader.py
+++ cli_song_downloader.py
@@ -13,36 +13,39 @@ print('[*]API_KEY from youtube data v3 API\n')
 print('[*]Googe API client python client\n')
 print('[*]youtube-dl for audio conversion')
 
-
-#checking dependencies
-def check_dependencies():
-	pkg[
-	'which curl'
-	'python3 -c apiclient'
+pkg=[
+	'which curl',
+	'python3 -c "import apiclient"',
 	'which youtube-dl'
 	]
 
-	exit_status = [len(pkg)]
+exit_status = ['0','0','0']
+print(len(exit_status))
+
+#checking dependencies
+def check_dependencies():
+	
 	ytdl_pkg = 'curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && chmod u+rx /usr/local/bin/youtube-dl'
-	for i in range(len(pkg)):
+	for i in range(3):
 		status = os.system(pkg[i])
+		print(status)
 		time.sleep(1)
 		exit_status[i] = os.system('echo $?')
 
 	for i in range(len(pkg)):
-		if(i==0 and exit_status[i]==1:)
+		if(i==0 and exit_status[i]==1):
 			print("cURL not found. terminating program\n")
 			print("install curl using\napt-get install curl")
 			sys.exit(1)
-		else if(i==0 and exit_status[i]==0):
+		elif(i==0 and exit_status[i]==0):
 			print("curl found.\nchecking for apiclient python3 library")
 
 		if(i==1 and exit_status[i]==1):
-			print("apiclient library for ppython3 not found")
+			print("apiclient library for python3 not found")
 			print("\ninstall it using 'python3 -m pip install apiclient'\n")
 			print("terminating program")
 			sys.exit(1)
-		else if(i==1 and exit_status[i]==0):
+		elif(i==1 and exit_status[i]==0):
 			print("apiclient python3 library found.\nchecking for youtube-dl package")
 
 		if(i==2 and exit_status[i]==1):
@@ -51,11 +54,13 @@ def check_dependencies():
 			print(ytdl_pkg)
 			print("Terminating now\n")
 			sys.exit(1)
-		else if(i==2 and exit_status[i]==0):
+		elif(i==2 and exit_status[i]==0):
 			print("youtube-dl package found.\nAll dependencies met.\n")
 
 
 print("\n[+]CHECKING DEPENDENCIES...\n")
+
+check_dependencies()
 
 				
 
@@ -78,3 +83,5 @@ download_cmd = 'youtube-dl '+link+' -o '+song_name
 print("\n")
 print("\n [+] DOWNLOADING SONG...\n")
 print(download_cmd)
+os.system(download_cmd)
+
