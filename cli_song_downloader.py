dea1a5e3c7ca444cd55b52d75e30d4bdce0546b9
mritunjay kumar mani <mkumarmani97@gmail.com>
2020-01-02 13:59:58 +0530

    Update cli_song_downloader.py

--- cli_song_downloader.py
+++ cli_song_downloader.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python3
 
-#!/usr/bin/python3
 from apiclient import discovery
 import os
-import time
 import sys
+import time
+def dependencies_list():
+	#satisfying dependencies
+	print('FOR THIS SCRIPT TO WORK FOLLOWING DEPENDENCIES MUST BE MET\n')
+	print('[*]cURL\n')
+	print('[*]Youtube data v3 API enabled\n')
+	print('[*]API_KEY from youtube data v3 API\n')
+	print('[*]Googe API client python client\n')
+	print('[*]youtube-dl for audio conversion')
 
-#satisfying dependencies
-print('FOR THIS SCRIPT TO WORK FOLLOWING DEPENDENCIES MUST BE MET\n')
-print('[*]cURL\n')
-print('[*]Youtube data v3 API enabled\n')
-print('[*]API_KEY from youtube data v3 API\n')
-print('[*]Googe API client python client\n')
-print('[*]youtube-dl for audio conversion')
 
 pkg=[
 	'which curl',
@@ -23,7 +24,7 @@ exit_status = ['0','0','0']
 
 #checking dependencies
 def check_dependencies():
-	
+
 	ytdl_pkg = 'curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && chmod u+rx /usr/local/bin/youtube-dl'
 	for i in range(3):
 		status = os.system(pkg[i])
@@ -57,43 +58,47 @@ def check_dependencies():
 			print("youtube-dl package found.\nAll dependencies met.\n")
 
 
-print("\n[+]CHECKING DEPENDENCIES...\n")
-check_dependencies()
-
-				
 #SONG DETAILS
-print("enter the number of song you want to download:\n")
-# song = str(input())
-n=int(input())
-song = []
-i=0
-print("enter the name of songs you want to download:\n")
-while(i<n):
-	song_name = str(input())
-	song.append(song_name)
-	i+=1
+def song_download():
+	print("enter the number of song you want to download:\n")
+	n=int(input())
+	if(n==0):
+		sys.exit(1)
+	else:
+		print("The program will download %d song"%n)
+	song = []
+	i=0
+	print("enter the name of songs you want to download:\n")
+	while(i<n):
+		song_name = str(input())
+		song.append(song_name)
+		i+=1
 
-print("The songs requested by the user are:\n")
-print(song)
-print('\n\n\n')
+	print("The songs requested by the user are:\n")
+	print(song)
+	print('\n\n\n')
 
 #OBTAINING SONG LINK FROM YOUTUBE
-api_key = 'PASTE THE YOUTUBE DATA V3 API KEY HERE' #PASTE THE YOUTUBE DATA V3 API KEY HERE.
-youtube = discovery.build('youtube','v3',developerKey=api_key)
-val=0
-while(val<len(song)):
-	search_query = youtube.search().list(part="snippet",maxResults=1,q=song[val])
-	response = search_query.execute()
-	var = response['items'][0]['id']['videoId']
-	link="https://www.youtube.com/watch?v="+var
+	api_key = 'AIzaSyCb2W5-at7jz3SKDHRELTwNb5AtBn7KBKs' #PASTE THE YOUTUBE DATA V3 API KEY HERE.
+	youtube = discovery.build('youtube','v3',developerKey=api_key)
+	val=0
+	while(val<len(song)):
+		search_query = youtube.search().list(part="snippet",maxResults=1,q=song[val])
+		response = search_query.execute()
+		var = response['items'][0]['id']['videoId']
+		link="https://www.youtube.com/watch?v="+var
 
-	#CONVERTING YOUTUBE VIDEO IN TO AUDIO AND DOWNLOADING IT.
-	song[val] = song[val].replace(" ", "_")
-	print("[+] DOWNLOADING SONG... \t%s\n" % song[val])
-	print('[+] THE SONG WILL BE SAVED in /root/Music/'+song[val]+'\n')
-	download_cmd = 'youtube-dl '+link+' -f bestaudio'+' -o /root/Music/'+song[val]
-	os.system(download_cmd)
-	print('\n\n\n')
-	val+=1
-	
+		#CONVERTING YOUTUBE VIDEO IN TO AUDIO AND DOWNLOADING IT.
+		song[val] = song[val].replace(" ", "_")
+		print("[+] DOWNLOADING SONG... \t%s\n" % song[val])
+		print('[+] THE SONG WILL BE SAVED in /root/Music/'+song[val]+'\n')
+		download_cmd = 'youtube-dl '+link+' -f bestaudio'+' -o /root/Music/'+song[val]
+		os.system(download_cmd)
+		print('\n\n\n')
+		os.system('ffmpeg -i '+song[val]+' '+song[val]+'.mp3')
+		os.system('rm /root/Music/'+song[val])
+		val+=1
 
+dependencies_list()
+check_dependencies()
+song_download()
